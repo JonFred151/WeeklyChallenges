bf105beced445639a36ff1e797884b18bda9a8d6
Jon Frederick <JonFred151@gmail.com>
2024-01-09 16:02:55 -0600

    update

--- ChallengesWithTestsMark8.Tests/obj/Debug/net6.0/ChallengesWithTestsMark8.Tests.AssemblyInfo.cs
+++ ChallengesWithTestsMark8.Tests/obj/Debug/net6.0/ChallengesWithTestsMark8.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChallengesWithTestsMark8.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+875de15ee4e665ab3c3b689a0ce5db94ced2f580")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44417f5212e7e126ababb02ece49db2cd88f9647")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChallengesWithTestsMark8.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChallengesWithTestsMark8.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ChallengesWithTestsMark8/ChallengesSet05.cs
+++ ChallengesWithTestsMark8/ChallengesSet05.cs
@@ -7,27 +7,77 @@ namespace ChallengesWithTestsMark8
     {
         public int GetNextNumberDivisibleByN(int startNumber, int n)
         {
-            throw new NotImplementedException();
+            return ((startNumber / n) + 1) * n;
         }
 
         public void ChangeNamesOfBusinessesWithNoRevenueTo_CLOSED(Business[] businesses)
         {
-            throw new NotImplementedException();
+            for (var i = 0; i < businesses.Length; i++)
+            {
+                if (businesses[i].TotalRevenue == 0)
+                {
+                    businesses[i].Name = "CLOSED";
+                }
+            }
         }
 
         public bool IsAscendingOrder(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Length == 0)
+            {
+                   return false;
+            }
+          for(var i = 0;i < numbers.Length -1 ;i++)
+            {
+                if (numbers[i]  > numbers[i + 1])
+                {
+                    return false; 
+                }
+               
+            }
+          return true;
         }
 
         public int SumElementsThatFollowAnEven(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Length == 0)
+            {
+                return 0;
+            }
+            int sum = 0;
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i-1] % 2 == 0)
+                {
+                    sum += numbers[i];
+                }
+                
+            }
+            return sum;
         }
 
         public string TurnWordsIntoSentence(string[] words)
         {
-            throw new NotImplementedException();
+            if (words == null || words.Length == 0)
+            {
+                return " ";
+            }
+            string combine = " ";
+           foreach(string word in words)
+            {
+                if(word.Trim().Length > 0)
+                {
+                     combine += word.Trim() + " ";
+                }
+                if (combine.Length == 0)
+                {
+                    return " ";
+                }
+               
+            }
+            combine = combine.Trim();
+            combine += ".";
+            return combine;
         }
 
         public double[] GetEveryFourthElement(List<double> elements)
